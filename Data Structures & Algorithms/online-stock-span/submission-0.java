1c2d3f2c2e2715da5e17a9019f6913eb02c6c455
Tarun Pusuluri <nani234917@gmail.com>
2026-09-25 23:03:54 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.java
@@ -0,0 +1,24 @@
+class StockSpanner {
+
+            // Each element stores {price, span}
+                private Stack<int[]> stack;
+
+                    public StockSpanner() {
+                            stack = new Stack<>();
+                                }
+
+                                    public int next(int price) {
+                                            int span = 1;
+
+                                                    // Merge previous spans while their prices
+                                                            // are less than or equal to today's price.
+                                                                    while (!stack.isEmpty() && stack.peek()[0] <= price) {
+                                                                                span += stack.pop()[1];
+                                                                                        }
+
+                                                                                                stack.push(new int[]{price, span});
+
+                                                                                                        return span;
+                                                                                                            }
+                                                                                                            
+}
